8620ad5c134a9b52f71288721324703ffc94d585
dilum1995 <dilumdesilva.edu@gmail.com>
2017-06-16 11:20:55 +0530

    db connection created

--- StudenInformationStoringApp/DALayer/connectionBuider.cs
+++ StudenInformationStoringApp/DALayer/connectionBuider.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DALayer
 {
     public class connectionBuider
     {
+        public static SqlConnection buildConection()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=nghq-it-023;Initial Catalog=dbtest;User ID=sa");
+                if (con.State == ConnectionState.Closed)
+                {
+                    
+                    con.Open();
+                }
+                else
+                {
+                    con.Close();
+                }
+
+                return con;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+         
+
     }
 }
